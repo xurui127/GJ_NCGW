9434085e8ec8eff0d68597b5bfbb95485c246533
Rui <xurui127@gmail.com>
2025-02-17 16:04:50 -0500

    Add planet shader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Transform ship;
+    [SerializeField] private GameObject vfxPrefab;
+    [SerializeField] private GameObject planet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
